ae5379766b2327738e0c23ff044ae550fd44cc2d
CFE <hello@teamcfe.com>
2018-04-04 14:56:32 -0700

    Final code

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -8,11 +8,17 @@ from django.conf.urls import include, url as re_path
 from django.urls import include, path, re_path
 from django.contrib import admin
 
+from pages.views import FrontendRenderView
+
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
     re_path(r'^api/products/', include("products.api.urls")),
+    # re_path(r'^api/abc/', include("abc.api.urls")),
+    # re_path(r'^api/cart/', include("cart.api.urls")),
 ]
 
 urlpatterns += [
     # your integrate path
+    re_path(r'(?P<path>.*)', FrontendRenderView.as_view(), name='home')
 ]
+# django no longer handles 404/403/500 errors; frontend does
--- src/pages/views.py
+++ src/pages/views.py
@@ -1,2 +1,7 @@
 from django.views.generic import View
 from django.shortcuts import render
+
+
+class FrontendRenderView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "pages/front-end-render.html", {})
